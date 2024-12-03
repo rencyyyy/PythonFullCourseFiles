4779eb81e47e3e7a1d64f9d6a39d1b262482885f
Rency Delos Santos <rencydelossantos020@gmail.com>
2024-12-03 10:33:51 +0800

    "NEW ADDED FILE | Request API Data"

--- Request API DATA.py
+++ Request API DATA.py
@@ -2,8 +2,8 @@ import requests
 
 base_url = "https://pokeapi.co/api/v2/"
 
-def get_pokemon_info(name):
-    url = f"{base_url}/pokemon/{name}"
+def get_pokemon_info(pokemon_name):
+    url = f"{base_url}/pokemon/{pokemon_name}"
     response = requests.get(url)
 
     if response.status_code == 200:
@@ -12,13 +12,15 @@ def get_pokemon_info(name):
     else:
         print(f"Failed to retrieve data {response.status_code}")
 
-pokemon_name = "okidogi"
+
+pokemon_name = "charizard"
 pokemon_info = get_pokemon_info(pokemon_name)
 
 if pokemon_info:
-    print(f"Pokemon name: {pokemon_info["name"].capitalize()}")
+    print(f"NAME: {pokemon_info["name"].capitalize()}")
     print(f"ID: {pokemon_info["id"]}")
-    print(f"Height: {pokemon_info["height"]}")
-    print(f"Weight: {pokemon_info["weight"]}")
+    print(f"BASE EXP: {pokemon_info["base_experience"]}")
+    print(f"HEIGHT: {pokemon_info["height"]}")
+    print(f"WEIGHT: {pokemon_info["weight"]}")
 
 
--- Test.py
+++ Test.py
@@ -768,9 +768,30 @@ import json
 # print("All task has completed")
 
 #-----------------------------------------------------------------------------------------------------------------------
+import requests
+
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon_info(name):
+    url = f"{base_url}/pokemon/{name}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+    else:
+        print(f"Data failed to retrieve {response.status_code}")
 
 
+pokemon_name = "pikachu"
+pokemon_info = get_pokemon_info(pokemon_name)
 
+if pokemon_info:
+    print(f"Name: {pokemon_info["name"]}")
+    print(f"ID: {pokemon_info["id"]}")
+    print(f"HEIGHT: {pokemon_info["height"]}")
+    print(f"WEIGHT: {pokemon_info["weight"]}")
+    print(f"EXP: {pokemon_info["base_experience"]}")
 
 
 
