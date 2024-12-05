62ed36b84d776ecf3c77e07bbadb23f93d17b456
Rency Delos Santos <rencydelossantos020@gmail.com>
2024-12-05 17:17:21 +0800

    "NEW ADDED FILE | PyQt5 Inserting Image as label"

---
+++ PyQt5 Directory/PyQt5_Image.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QIcon, QPixmap
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+        self.setWindowTitle("Inserting Image")
+        self.setWindowIcon(QIcon("Images/icon.jpg"))
+
+        label = QLabel(self)
+        label.setGeometry(0, 0, 250, 250)
+
+        pixmap = QPixmap("Images/Insert_Image.jpg")
+        label.setPixmap(pixmap)
+
+        label.setScaledContents(True)
+
+        label.setGeometry((self.width() - label.width()) // 2,
+                          (self.height() - label.height()) // 2,
+                          label.width(),
+                          label.height())
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
