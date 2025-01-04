759ea4f865a82ec3919ebf03676fb30fac1e03d8
Rency Delos Santos <rencydelossantos020@gmail.com>
2025-01-04 11:25:16 +0800

    "New file added | Object as argument sample"

--- Object_as_ARGUMENT.py
+++ Object_as_ARGUMENT.py
@@ -1,6 +1,8 @@
 class Car:
     color = None
 
+class Motorcycle:
+    color = None
 def change_color(car, color):
     car.color = color
 
@@ -8,10 +10,21 @@ car_1 = Car()
 car_2 = Car()
 car_3 = Car()
 
+motorcycle_1 = Motorcycle()
+motorcycle_2 = Motorcycle()
+motorcycle_3 = Motorcycle()
+
 change_color(car_1, "red")
 change_color(car_2, "blue")
 change_color(car_3, "yellow")
 
+change_color(motorcycle_1, "orange")
+change_color(motorcycle_2, "green")
+change_color(motorcycle_3, "purple")
+
 print(car_1.color)
 print(car_2.color)
 print(car_3.color)
+print(motorcycle_1.color)
+print(motorcycle_2.color)
+print(motorcycle_3.color)
