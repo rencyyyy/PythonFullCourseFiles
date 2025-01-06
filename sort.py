613df3c3c86b3f7ba879194d9f7e2b0b7c8b4024
Rency Delos Santos <rencydelossantos020@gmail.com>
2025-01-06 11:28:17 +0800

    Sort Method & Sort Function Added

--- sort.py
+++ sort.py
@@ -1,7 +1,13 @@
 # sort() method = used with lists
 # sort() function = used with iterables
-
+# REVERSE NOTE: sort method: sort(reverse=True)
+#               sort function: object = sorted(list, reverse=True)
+#
+# LEVEL 1
+print("\nLEVEL 1")
+#-----------------------------------------------------------------------------------------------------------------------
 # Sort method: Sorts the list in alphabetical order
+print("\nSort Method")
 students = ["Anne", "Barbie", "Charlie", "Dante", "Emilio"]
 
 students.sort()
@@ -11,6 +17,7 @@ for student in students:
     print(student)
 
 # Sort function: Sorts the list in alphabetical ordered
+print("\nSort function")
 
 employees = ["Rency", "Jose", "Pedro", "Angelica", "Gon"]
 
@@ -18,3 +25,55 @@ sorted_employees = sorted(employees)
 # sorted_employees = sorted(employees, reverse=True) # sort a list by using a function sorted (reversed order)
 for i in sorted_employees:
     print(i)
+
+
+# LEVEL 2
+print("\nLEVEL 2")
+#-----------------------------------------------------------------------------------------------------------------------
+
+# List of tuples
+
+players = [("Ryukaji", "A", 72),
+           ("Nard", "B", 70),
+           ("Albert", "C", 64),
+           ("Blood", "D", 58),
+           ("Cyren", "C", 60)]
+# Sort method
+print("\nSort by name (normal)")
+players.sort()
+# players.sort(reverse=True)
+for i in players:
+    print(i)
+
+# Sort based on skill level (2nd index the list)
+print("\nSort based on skill level")
+skill_level = lambda level: level[1]
+players.sort(key=skill_level)
+
+for i in players:
+    print(i)
+
+# Sort based on KD ratio (3rd index of the list)
+print("\nSort based on skill KD Ratio percentage (reversed)")
+kd = lambda ratio:ratio[2]
+players.sort(key=kd, reverse=True)
+
+for i in players:
+    print(i)
+
+# Tuples of tuples
+print("\nSORT TUPLES OF TUPLES BASED ON KD RATIO (reversed)")
+players = (("Ryukaji", "A", 72),
+           ("Nard", "B", 70),
+           ("Albert", "C", 64),
+           ("Blood", "D", 58),
+           ("Cyren", "C", 60))
+
+# Sort tuples of tuples based on KD Ratio
+kd_ratio = lambda rate: rate[2]
+sorted_player = sorted(players, key=kd_ratio, reverse=True)
+
+for i in sorted_player:
+    print(i)
+
+
